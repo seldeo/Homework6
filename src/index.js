9f28a2e06294ddc6cf62f8e135689fda62caeacc
unknown <selinaadwoa1@gmail.com>
2024-11-29 13:03:49 -0800

    made changes

---
+++ src/index.js
@@ -0,0 +1,31 @@
+let name = "Selina";
+console.log(name);
+
+let age = 12;
+console.log(age);
+
+let food = 5+8
+console.log(food);
+
+let girls = 20-5
+console.log(girls);
+
+let boys = 12/18
+console.log(boys);
+
+let father = 5*9
+console.log(father);
+
+let mothers = 7*9
+console.log(mothers);
+
+
+let fufu = true
+console.log(fufu);
+
+let peanutsoup = 15
+console.log(number);
+
+let chicken = ("everybody");
+console.log (:"string");
+
