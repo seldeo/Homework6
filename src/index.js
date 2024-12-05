5523fb4c68e818b696c4e155baba9723bdd0c2f8
unknown <selinaadwoa1@gmail.com>
2024-12-05 10:03:01 -0800

    adding all folder

--- src/index.js
+++ src/index.js
@@ -1,31 +1,69 @@
-let name = "Selina";
+//let name = "Selina";
+//console.log(name);
+
+//let age = 12;
+//console.log(age);
+
+//let food = 5+8
+//console.log(food);
+
+//let girls = 20-5
+//console.log(girls);
+
+//let boys = 12/18
+//console.log(boys);
+
+//let father = 5*9
+//console.log(father);
+
+//let mothers = 7*9
+//console.log(mothers);
+
+
+//let fufu = true
+//console.log(fufu);
+
+//let peanutsoup = 15
+//console.log(number);
+
+//let chicken = ("everybody");
+//console.log ("string");
+
+//let name
+
+let name = ("Selina");
 console.log(name);
-
-let age = 12;
-console.log(age);
-
-let food = 5+8
-console.log(food);
-
-let girls = 20-5
-console.log(girls);
-
-let boys = 12/18
-console.log(boys);
-
-let father = 5*9
-console.log(father);
-
-let mothers = 7*9
-console.log(mothers);
-
-
-let fufu = true
-console.log(fufu);
-
-let peanutsoup = 15
-console.log(number);
-
-let chicken = ("everybody");
-console.log (:"string");
+let Age = (89);
+console.log(Age);
+let num1 = (10);
+console.log(num1);
+let num2 = (15);
+console.log(num2);
+let sum = (num1+num2);
+console.log(sum);
+let num3 = (20);
+console.log(num3);
+let num4 = (5);
+console.log(num4);
+let diffrence = (num3-num4);
+console.log(difference);
+let num5 = (50);
+console.log(num5);
+let num6 = (5);
+console.log(num6);
+let quotient = (num5/num6);
+console.log(quotient);
+let num7 = (7);
+console.log(num7);
+let num8 = (6);
+console.log(num8);
+let product = (num7*num8);
+console.log(product);
+// let remainder = 15 * 4;
+// console.log(remainder);
+// let student = false;
+// let variable = 150;
+// console.log(typeof (variable));
+// variable = "Hellow, Where is my money?";
+// console.log(typeof (variable));
 
